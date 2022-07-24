444608888dfd56723840245cb134de1c517aecae
dhanie-ellah <101949297+dhanie-ellah@users.noreply.github.com>
2022-07-24 15:14:39 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let homeScore = document.getElementById('homeScore');
+let guestScore = document.getElementById('guestScore');
+let i = 0;
+let j = 0;
+
+function homePlusOne(){
+    i += 1;
+    homeScore.textContent = i;
+}
+function homePlusTwo(){
+    i += 2;
+    homeScore.textContent = i;
+}
+function homePlusThree(){
+    i += 3;
+    homeScore.textContent = i;
+}
+
+function guestPlusOne(){
+    j += 1;
+    guestScore.textContent = j;
+}
+function guestPlusTwo(){
+    j += 2;
+    guestScore.textContent = j;
+}
+function guestPlusThree(){
+    j += 3;
+    guestScore.textContent = j;
+}
